c1cbd10a25c808c74573f98498fc7ea84ecb7c2c
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2019-10-16 12:57:51 +0200

    Added time function

--- load.js
+++ load.js
@@ -4,3 +4,10 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("mainDiv").style.display = "block";
 }
+
+function displayDate() {
+  var x = new Date();
+  var x1=x.toUTCString;
+  document.getElementById('date').innerHTML = x1;
+  tt=display_c();
+}
