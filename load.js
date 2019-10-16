8775e049b0d7cedc005951ac11bb823609b58451
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2019-10-16 13:07:38 +0200

    removed useless scripts

--- load.js
+++ load.js
@@ -4,15 +4,3 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("mainDiv").style.display = "block";
 }
-
-function displayC() {
-  var refresh = 1000;
-  myTime=setTimeout('displayDate()', refresh)
-}
-
-function displayDate() {
-  var x = new Date();
-  var x1=x.toUTCString;
-  document.getElementById('date').innerHTML = x1;
-  tt=displayC();
-}
