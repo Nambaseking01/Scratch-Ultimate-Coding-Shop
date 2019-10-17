829e8cb5fb66df06d3b75090c2e76b37e49a0212
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2019-10-17 17:40:28 +0200

    Update load.js

--- load.js
+++ load.js
@@ -4,7 +4,3 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("mainDiv").style.display = "block";
 }
-
-var m = "Last updated on " + document.lastModified;
-var p = m.length-8;
-document.getElementById("last_updated").innerHTML = m.substring(p, 0);
