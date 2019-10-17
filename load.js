d98b6150b1263a3e4e1ce8a98a99b56be3144ea6
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2019-10-17 17:32:26 +0200

    Trying to resolve issue #14

--- load.js
+++ load.js
@@ -4,3 +4,7 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("mainDiv").style.display = "block";
 }
+
+var m = "Last updated on " + document.lastModified;
+var p = m.length-8;
+document.getElementById("lastUpdated").innerHTML = m.substring(p, 0);
