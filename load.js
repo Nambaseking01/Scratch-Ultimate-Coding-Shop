69ce346faa2ced640e6633d46037b3de24f40e33
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2019-10-18 22:46:40 +0200

    moving clock function to other file

--- load.js
+++ load.js
@@ -4,19 +4,3 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("mainDiv").style.display = "block";
 }
-
-function updateClock {
-  var now = new Date(), 
-      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-  
-  date = [now.getDate(),
-          months[now.getMonth()],
-          now.getFullYear()].join(' ');
-  
-  document.getElementById('time').innerHTML = [date, time].join('/');
-  
-  setTimeout(updateClock, 1000;)
-}
-
-setTimeout(updateClock, 501)
