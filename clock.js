23e7dc1c35c8cf8b399e9d9817d39af0b5774dba
Nammy <54203371+Nambaseking01@users.noreply.github.com>
2019-10-18 22:47:09 +0200

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,15 @@
+function updateClock {
+  var now = new Date(), 
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+  
+  date = [now.getDate(),
+          months[now.getMonth()],
+          now.getFullYear()].join(' ');
+  
+  document.getElementById('time').innerHTML = [date, time].join('/');
+  
+  setTimeout(updateClock, 1000;)
+}
+
+updateClock();
