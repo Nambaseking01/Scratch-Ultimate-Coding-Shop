1345642ebb3340516961da03885c740028d4197e
Smiley <git@smileycreations15.com>
2019-10-19 13:10:08 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -3,13 +3,13 @@
 function updateClock() {
   var now = new Date(), 
       months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  time = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+  time = now.getHours().toString().padStart(2,"0") + ':' + now.getMinutes().toString().padStart(2,"0") + ':' + now.getSeconds().toString().padStart(2,"0");
   
   date = [now.getDate(),
           months[now.getMonth()],
           now.getFullYear()].join(' ');
   
-  document.getElementById('time').innerHTML = [date, time].join('/');
+  document.getElementById('time').innerHTML = [date, time].join(' / ');
   
   setTimeout(updateClock, 1000);
 }
